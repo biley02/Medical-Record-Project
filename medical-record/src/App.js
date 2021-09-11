30f0397dba9a70740b66b50d53923f0981a1bf92
Rishiraj Kalita <rishirajkalita13@gmail.com>
2021-09-11 21:46:48 +0530

    made navbar added router

--- medical-record/src/App.js
+++ medical-record/src/App.js
@@ -1,12 +1,20 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Home from "./components/Home";
+import Navbar from "./components/Navbvar";
+
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom'
+
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router className="App">
+      <Navbar/>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch> 
+    </Router> 
   );
 }
 
